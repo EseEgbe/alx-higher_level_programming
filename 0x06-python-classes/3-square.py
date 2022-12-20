71bcc6a7748fed9061e974fafc8beb5a61949f5a
EseEgbe <111199241+EseEgbe@users.noreply.github.com>
2022-12-21 00:20:09 +0100

    Project tasks

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-class Square:
-    """A class that defines a square by its size"""
+"""Defines a class Square"""
 
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
-        """Method to initialize the square object"""
-        if not isinstance(size, int):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-       else:
-            self.__size = size
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
 
     def area(self):
-        """Method that returns the square are of the object"""
-        return self.__size**2
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
